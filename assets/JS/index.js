3f486adbb18bfd221fe94edf8dc9aabe50b69f83
Archangel <nathapcal@gmail.com>
2020-09-18 00:22:31 -0400

    finished connecting buttons. added footer and extra styling

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,27 +1,85 @@
 $(document).ready(function () {
-// Dom variables
+  // Dom variables
 
-    var tonedBtn = $("#tonedTribeClick")
-    var tonedTribe = $("#tonedTribe")
-    var aboutMe = $("#aboutMe")
-    var homeBtn = $("#homeBtn")
-// JS variables
+  var tonedBtn = $("#tonedTribeClick");
+  var tonedTribe = $("#tonedTribe");
+  var aboutMe = $("#aboutMe");
+  var homeBtn = $("#homeBtn");
+  var planner = $("#planner");
+  var genPass = $("#genPass");
+  var plannerBtn = $("#plannerClick");
+  var genPassBtn = $("#genPassClick");
+  var contacts = $("#contacts");
+  var contactClick = $("#contactClick");
+  var motivational = $("#motivational");
+  var motivationalClick = $("#motivationalClick");
 
-// functions
+  // JS variables
 
-// call functions
+  // functions
 
-// event listeners
-tonedBtn.on("click", function(event){
-    event.preventDefault()
-    aboutMe.addClass("hide")
-    tonedTribe.removeClass("hide")
-})
+  // call functions
 
-homeBtn.on("click", function(event){
-    event.preventDefault()
-    aboutMe.removeClass("hide")
-    tonedTribe.addClass("hide")
-})
+  // event listeners
 
+  // home button
+  homeBtn.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.removeClass("hide");
+    tonedTribe.addClass("hide");
+    planner.addClass("hide");
+    genPass.addClass("hide");
+    contacts.addClass("hide");
+    motivational.addClass("hide");
+  });
+  //planner button
+  tonedBtn.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.addClass("hide");
+    tonedTribe.removeClass("hide");
+    planner.addClass("hide");
+    genPass.addClass("hide");
+    contacts.addClass("hide");
+    motivational.addClass("hide");
+  });
+  //planner button
+  plannerBtn.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.addClass("hide");
+    planner.removeClass("hide");
+    tonedTribe.addClass("hide");
+    genPass.addClass("hide");
+    contacts.addClass("hide");
+    motivational.addClass("hide");
+  });
+  // GenPass button
+  genPassBtn.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.addClass("hide");
+    tonedTribe.addClass("hide");
+    planner.addClass("hide");
+    genPass.removeClass("hide");
+    contacts.addClass("hide");
+    motivational.addClass("hide");
+  });
+  // Contact Button
+  contactClick.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.addClass("hide");
+    tonedTribe.addClass("hide");
+    planner.addClass("hide");
+    genPass.addClass("hide");
+    contacts.removeClass("hide");
+    motivational.addClass("hide");
+  });
+  // Motivation button
+  motivationalClick.on("click", function (event) {
+    event.preventDefault();
+    aboutMe.addClass("hide");
+    tonedTribe.addClass("hide");
+    planner.addClass("hide");
+    genPass.addClass("hide");
+    contacts.addClass("hide");
+    motivational.removeClass("hide");
+  });
 });
