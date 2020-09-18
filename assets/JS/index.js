11a67c3e8d85de609e0e2f5cd37cfcc6128387af
Archangel <nathapcal@gmail.com>
2020-09-17 23:11:10 -0400

    added buttons to switch between home and project1 section

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+$(document).ready(function () {
+// Dom variables
+
+    var tonedBtn = $("#tonedTribeClick")
+    var tonedTribe = $("#tonedTribe")
+    var aboutMe = $("#aboutMe")
+    var homeBtn = $("#homeBtn")
+// JS variables
+
+// functions
+
+// call functions
+
+// event listeners
+tonedBtn.on("click", function(event){
+    event.preventDefault()
+    aboutMe.addClass("hide")
+    tonedTribe.removeClass("hide")
+})
+
+homeBtn.on("click", function(event){
+    event.preventDefault()
+    aboutMe.removeClass("hide")
+    tonedTribe.addClass("hide")
+})
+
+});
